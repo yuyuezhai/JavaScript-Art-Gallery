b03bd655c9b64f721739aaafdab5e51b5fdebefe
yuyuezhai <nhzyy@nuaa.edu.cn>
2021-02-18 16:14:40 +0800

    Art Gallery

---
+++ Gallery2/scripts/showPic.js
@@ -0,0 +1,42 @@
+function showPic(whichpic){
+	if(!document.getElementById("placeholder")) return true;
+	var source = whichpic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	placeholder.setAttribute("src",source);
+	if(!document.getElementById("description")) return false;
+	var text=whichpic.getAttribute("title");
+	document.getElementById("description").innerHTML=text;
+	return false;
+}
+
+function prepareGallery(){
+	if(!document.getElementsByTagName || !document.getElementById){
+		return false;
+	}
+	if(!document.getElementById("imagegallery")){
+		return false;
+	}
+	var gallery=document.getElementById("imagegallery");
+	var links=gallery.getElementsByTagName("a");
+	for(var i=0;i<links.length;i++){
+		links[i].onclick=function(){
+			return showPic(this);
+		}
+	}
+}
+function addLoadEvent(func)
+{
+    var oldonload=window.onload;
+    if(typeof window.onload!="function")
+    {
+        window.onload=func;
+    }
+    else
+    {
+        window.onload=function(){
+            oldonload();
+            func();
+        }
+    }
+}
+addLoadEvent(prepareGallery);
